6f9690458da2480603ee1fc84145027a252ad6e8
Victor <14256421+gaeshi@users.noreply.github.com>
2022-10-08 00:22:34 +0900

    Implement users-actions-panel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -24,8 +29,13 @@ import { UsersListComponent } from './users-container/users-list/users-list.comp
     BrowserAnimationsModule,
     BrowserModule,
     FlexLayoutModule,
+    MatButtonModule,
+    MatCheckboxModule,
     MatDividerModule,
+    MatIconModule,
+    MatInputModule,
     MatListModule,
+    ReactiveFormsModule,
   ],
   providers: [],
 })
--- src/app/users-container/users-actions-panel/users-actions-panel.component.ts
+++ src/app/users-container/users-actions-panel/users-actions-panel.component.ts
@@ -1,4 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Subject, debounceTime, distinctUntilChanged, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-users-actions-panel',
@@ -6,4 +16,35 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./users-actions-panel.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UsersActionsPanelComponent {}
+export class UsersActionsPanelComponent implements OnInit, OnDestroy {
+  @Input() deleteDisabled = true;
+  @Output() emitSelectAll = new EventEmitter<boolean>();
+  @Output() emitSearch = new EventEmitter<string>();
+
+  unsubscribe = new Subject<void>();
+
+  form = new FormGroup({
+    selectAll: new FormControl(false, { nonNullable: true }),
+    search: new FormControl('', { nonNullable: true }),
+  });
+
+  selectAllChanges$ = this.form.get('selectAll')!.valueChanges;
+  searchChanges$ = this.form.get('search')!.valueChanges;
+
+  ngOnInit(): void {
+    this.selectAllChanges$
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe((value) => this.emitSelectAll.emit(value));
+    this.searchChanges$
+      .pipe(
+        takeUntil(this.unsubscribe),
+        debounceTime(200),
+        distinctUntilChanged()
+      )
+      .subscribe((value) => this.emitSearch.emit(value));
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+  }
+}
